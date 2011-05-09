b86d3377bd6219f57c2de0c3bbeaf2d0fd7d689f
Roger Barnes <roger@mindsocket.com.au>
2011-05-10 08:48:33 +1000

    lint fixes

--- apps/fusion/tests.py
+++ apps/fusion/tests.py
@@ -3,46 +3,47 @@ from apps.fusion.models import Fusion, Image, ImageAligner
 import unittest2 as unittest
 
 def mocknowimage():
-    i = Image()
-    i.imageurl = "http://nowandthen.mindsocket.com.au/media/cache/fd/fd86c45beed791eda0b5fc50ccbbab71.jpg"
-    return i
+    image = Image()
+    image.imageurl = "http://nowandthen.mindsocket.com.au/media/cache/fd/fd86c45beed791eda0b5fc50ccbbab71.jpg"
+    return image
 
 def mockthenimage():
-    i = Image()
-    i.imageurl = "http://nowandthen.mindsocket.com.au/media/cache/31/31ec06ffad82a92abd648e46fab54a44.jpg"
-    return i
+    image = Image()
+    image.imageurl = "http://nowandthen.mindsocket.com.au/media/cache/31/31ec06ffad82a92abd648e46fab54a44.jpg"
+    return image
 
 def mockfusion():
-    f = Fusion()
-    f.id = 999
-    f.now = mocknowimage()
-    f.then = mockthenimage()
-    f.points = "0,0,0,0,100,100,150,150"
-    return f
+    fusion = Fusion()
+    fusion.id = 999
+    fusion.now = mocknowimage()
+    fusion.then = mockthenimage()
+    fusion.points = "0,0,0,0,100,100,150,150"
+    return fusion
 
 class TestFusion(TestCase):
     """Basic tests for the fusion class"""
     
     def test_point_list(self):
         """Tests that point_list returns a valid list of lists"""
-        f = Fusion()
-        f.points = "1,2,3,4,5,6,7,8"
-        self.failUnlessEqual(f.point_list(), [[1,2,3,4],[5,6,7,8]])
+        fusion = Fusion()
+        fusion.points = "1,2,3,4,5,6,7,8"
+        self.failUnlessEqual(fusion.point_list(), [[1,2,3,4],[5,6,7,8]])
 
     def test_empty_point_list(self):
         """Tests that point_list returns a valid list of lists"""
-        f = Fusion()
-        f.points = ""
-        self.failUnlessEqual(f.point_list(), [])
+        fusion = Fusion()
+        fusion.points = ""
+        self.failUnlessEqual(fusion.point_list(), [])
         
     def test_image_aligner(self):
         """Tests that the image aligner does something useful - ie aligns images"""
-        f = mockfusion()
-        a = ImageAligner(f)
-        a.align("test")
+        fusion = mockfusion()
+        aligner = ImageAligner(fusion)
+        aligner.align("test")
 
     @unittest.skip("not needed now")
     def test_pto_generation(self):
-        f = mockfusion()
-        a = ImageAligner(f)
-        print(a.get_pto_string())
+        fusion = mockfusion()
+        aligner = ImageAligner(fusion)
+        print(aligner.get_pto_string())
+
