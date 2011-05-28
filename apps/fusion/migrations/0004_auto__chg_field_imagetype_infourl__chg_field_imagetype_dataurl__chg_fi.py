2507b983ef5dbeb4a72afe05c57e1e66864b7b2f
Roger Barnes <roger@mindsocket.com.au>
2011-05-29 04:08:06 +1000

    shortened url migration

---
+++ apps/fusion/migrations/0004_auto__chg_field_imagetype_infourl__chg_field_imagetype_dataurl__chg_fi.py
@@ -0,0 +1,124 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'ImageType.infourl'
+        db.alter_column('fusion_imagetype', 'infourl', self.gf('django.db.models.fields.URLField')(max_length=255))
+
+        # Changing field 'ImageType.dataurl'
+        db.alter_column('fusion_imagetype', 'dataurl', self.gf('django.db.models.fields.URLField')(max_length=255))
+
+        # Changing field 'Image.infourl'
+        db.alter_column('fusion_image', 'infourl', self.gf('django.db.models.fields.URLField')(max_length=255))
+
+        # Changing field 'Image.thumburl'
+        db.alter_column('fusion_image', 'thumburl', self.gf('django.db.models.fields.URLField')(max_length=255))
+
+        # Changing field 'Image.imageurl'
+        db.alter_column('fusion_image', 'imageurl', self.gf('django.db.models.fields.URLField')(unique=True, max_length=255))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'ImageType.infourl'
+        db.alter_column('fusion_imagetype', 'infourl', self.gf('django.db.models.fields.URLField')(max_length=256))
+
+        # Changing field 'ImageType.dataurl'
+        db.alter_column('fusion_imagetype', 'dataurl', self.gf('django.db.models.fields.URLField')(max_length=256))
+
+        # Changing field 'Image.infourl'
+        db.alter_column('fusion_image', 'infourl', self.gf('django.db.models.fields.URLField')(max_length=256))
+
+        # Changing field 'Image.thumburl'
+        db.alter_column('fusion_image', 'thumburl', self.gf('django.db.models.fields.URLField')(max_length=256))
+
+        # Changing field 'Image.imageurl'
+        db.alter_column('fusion_image', 'imageurl', self.gf('django.db.models.fields.URLField')(max_length=256, unique=True))
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'fusion.fusion': {
+            'Meta': {'object_name': 'Fusion'},
+            'cropthen': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '20', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '150', 'blank': 'True'}),
+            'flagged': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hide': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'now': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'now'", 'to': "orm['fusion.Image']"}),
+            'nowfile': ('django.db.models.fields.FilePathField', [], {'max_length': '100', 'path': "'/usr/local/src/nowandthen/media/fusions'", 'null': 'True', 'blank': 'True'}),
+            'points': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '512', 'blank': 'True'}),
+            'publish': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'then': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'then'", 'to': "orm['fusion.Image']"}),
+            'thenfile': ('django.db.models.fields.FilePathField', [], {'max_length': '100', 'path': "'/usr/local/src/nowandthen/media/fusions'", 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['auth.User']"})
+        },
+        'fusion.image': {
+            'Meta': {'object_name': 'Image'},
+            'creator': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'dateofwork': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'hide': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imageurl': ('django.db.models.fields.URLField', [], {'unique': 'True', 'max_length': '255'}),
+            'infourl': ('django.db.models.fields.URLField', [], {'max_length': '255', 'blank': 'True'}),
+            'sourcesystemid': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'thumburl': ('django.db.models.fields.URLField', [], {'max_length': '255', 'blank': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['fusion.ImageType']"})
+        },
+        'fusion.imagetype': {
+            'Meta': {'object_name': 'ImageType'},
+            'canbethen': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'dataurl': ('django.db.models.fields.URLField', [], {'max_length': '255', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'hide': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'infourl': ('django.db.models.fields.URLField', [], {'max_length': '255', 'blank': 'True'}),
+            'longdescription': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'sourcesystemid': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'typename': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'})
+        }
+    }
+
+    complete_apps = ['fusion']
