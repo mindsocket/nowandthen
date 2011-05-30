d9f6291810a4a74db770d83cbd7a7a8008c2a2e8
Roger Barnes <roger@mindsocket.com.au>
2011-05-30 23:13:39 +1000

    Trunc long descriptions

--- apps/fusion/jobs/load_library_data.py
+++ apps/fusion/jobs/load_library_data.py
@@ -40,6 +40,8 @@ class Job(BaseJob):
     help = "Data import job for sydney pictures"
 
     def execute(self):
+        #pylint: disable-msg=E1101
+        Image.tags.all().delete()
         Image.objects.filter(type__typename='SYDpre1885').delete()
         datafile = open(os.path.join(settings.DATA_ROOT, 'NSW', 'sydney-pictures.csv'), 'rb')
         reader = UnicodeReader(datafile, dialect='excel', encoding='latin1')
@@ -49,15 +51,14 @@ class Job(BaseJob):
         """412069,"Sydney - photographs of streets, public buildings, views in the Harbour, suburbs etc., chiefly pre 1885","The City Bank [corner of King & George Streets, Sydney, ca. 1870s]",NULL,412050,[ca. 1870s],http://acms.sl.nsw.gov.au/item/itemdetailpaged.aspx?itemid=412050,http://acms.sl.nsw.gov.au/item/itemdetailpaged.aspx?itemid=412069,a089002,http://acms.sl.nsw.gov.au/_DAMt/image/18/122/a089002t.jpg,http://acms.sl.nsw.gov.au/_DAMx/image/18/122/a089002h.jpg,NULL,1"""
         reader.next()
 #        lastimage = None
-        #pylint: disable-msg=E1101
         imagetype = ImageType.objects.get(typename='SYDpre1885')
         for row in reader:
-#            print(row)
+            print(row)
             try:
                 #pylint: disable-msg=W0612
                 image, created = Image.objects.get_or_create(type=imagetype, 
                                         imageurl=row[10], thumburl=row[9], infourl=row[7],
-                                        description=row[2], sourcesystemid=row[8],
+                                        description=row[2][:150], sourcesystemid=row[8],
                                         creator=row[3][:32], dateofwork=row[5][:32])
             
                 image.tags = ",".join(extract_words(row[2]))
