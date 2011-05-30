70fadea0a6231837faf49b591eec65bec9b645c2
Roger Barnes <roger@mindsocket.com.au>
2011-05-30 22:49:39 +1000

    Media updates: upgraded jquery added cycle plugin added jquery ui and theme removed profiling

--- media/js/script.js
+++ media/js/script.js
@@ -1,26 +1,41 @@
-function vote(type, id, direction, csrf_token) {
-    $.post('/'+type+'/'+id+'/'+direction+'vote/', {HTTP_X_REQUESTED:'XMLHttpRequest', csrfmiddlewaretoken:csrf_token},
-           function(data) {
-               if (data.success == true) {
-                   $('#score').text(data.score.score);
-//                   $('#num_votes').text(data.score.num_votes);
-               } else {
-                   alert('ERROR: ' + data.error_message);
-               }
-           }, 'json'
-          );
-}
-
-
 $(function() {
+	
+	$('#tabs').tabs();
+
 	$(".votes a.vote").click(
 		function(event) {
-			type = $(this).parent().find('input[name=type]').val()
-			object_id = $(this).parent().find('input[name=object_id]').val()
-			csrf_token = $(this).parent().find('input[name=csrf_token]').val()
-			vote(type, object_id, 'up', csrf_token);
-			$(this).removeClass('vote-up-off').addClass('vote-up-on');
-			$(this).removeClass('vote-up-off').addClass('vote-up-on');
+			parent = $(this).parent();
+			type = parent.find('input[name=type]').val();
+			current_score = $('#score').text;
+			if ($(this).hasClass("vote-up-off")) {
+				direction = 'up';
+			} else if ($(this).hasClass("vote-up-on") || $(this).hasClass("vote-down-on")) {
+				direction = 'clear';
+			} else if ($(this).hasClass("vote-down-off")) {
+				direction = 'down';
+			}
+			object_id = parent.find('input[name=object_id]').val();
+			csrf_token = parent.find('input[name=csrf_token]').val();
+			$.post('/'+type+'/'+object_id+'/'+direction+'vote/', {HTTP_X_REQUESTED:'XMLHttpRequest', csrfmiddlewaretoken:csrf_token},
+				function(data) {
+					if (data.success) {
+						$('#score').text(data.score.score);
+					// $('#num_votes').text(data.score.num_votes);
+						if (direction === "up") {
+							parent.find('.vote-up-off').removeClass('vote-up-off').addClass('vote-up-on');
+							parent.find('.vote-down-on').removeClass('vote-down-on').addClass('vote-down-off');
+						} else if (direction === "down") {
+							parent.find('.vote-up-on').removeClass('vote-up-on').addClass('vote-up-off');
+							parent.find('.vote-down-off').removeClass('vote-down-off').addClass('vote-down-on');
+						} else  {
+							parent.find('.vote-up-on').removeClass('vote-up-on').addClass('vote-up-off');
+							parent.find('.vote-down-on').removeClass('vote-down-on').addClass('vote-down-off');
+						}
+					} else {
+						alert('ERROR: ' + data.error_message);
+					}
+				}, 'json'
+        	);
 		}
 	);
 });
