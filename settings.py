9e235157272f22008096e85e44a78cd67ccc023e
Roger Barnes <roger@mindsocket.com.au>
2011-06-20 23:22:13 +1000

    Fixed ROOT_URLCONF so that project name isn't included as module name

--- settings.py
+++ settings.py
@@ -111,7 +111,7 @@ MIDDLEWARE_CLASSES = [
 #    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = "nowandthen.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATE_DIRS = [
     os.path.join(PROJECT_ROOT, "templates"),
