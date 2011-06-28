fb8d26a30cf5d91c944df1cf0a6d3f3816eb4ed6
Roger Barnes <roger@mindsocket.com.au>
2011-06-28 22:23:53 +1000

    Test improvements, better URL coverage

--- apps/fusion/tests.py
+++ apps/fusion/tests.py
@@ -37,21 +37,59 @@ class TestFusion(TestCase):
         fusion = mockfusion()
         aligner = ImageAligner(fusion)
         print(aligner.get_pto_string())
+        
+    def _test_page(self, url, string=None):
+        response = self.client.get(url)
+        if string:
+            self.assertContains(response, string, status_code=200)
+        else:
+            self.assertEqual(response.status_code, 200)
+            
+    def _login(self):
+        response = self.client.post('/account/login/', {'username': 'testuser', 'password': 'test'})
+        self.assertEqual(response.status_code, 302)
 
     def test_homepage(self):
-        response = self.client.get('/')
-        self.assertContains(response, "Welcome", status_code=200)
+        self._test_page('/', 'Welcome')
         
     def test_fusion_search(self):
-        response = self.client.get('/fusions')
-        self.assertContains(response, "Fusion List", status_code=200)
+        self._test_page('/fusions', "Fusion List")
         
     def test_fusion_feed(self):
-        response = self.client.get('/fusions/rss')
-        self.assertContains(response, "Now and Then Latest Fusions", status_code=200)
+        self._test_page('/fusions/rss', "Now and Then Latest Fusions")
         
     def test_image_search(self):
-        response = self.client.get('/images')
-        self.assertContains(response, "Image List", status_code=200)
+        self._test_page('/images', "Image List")
         
-                
+    def test_about(self):
+        self._test_page('/about/', 'beer!')
+        
+    def test_openid_page(self):
+        self._test_page("/openid/")
+        
+    def test_image_view(self):
+        self._test_page('/image/view/873/slug', 'viewimage')
+        
+    def test_fusion_new(self):
+        self._login()
+        self._test_page('/fusion/new/873/', 'nowandthen tag')
+        
+    def test_fusion_create(self):
+        self._login()
+        self._test_page('/fusion/create/873/5518762196/', 'control_point_panel')
+
+    def test_fusion_view(self):
+        self._test_page('/fusion/view/1/', 'Fusion by')
+
+    def test_fusion_edit(self):
+        self._login()
+        self._test_page('/fusion/edit/1/', 'control_point_panel')
+
+    def test_image_map_xml(self):
+        self._test_page('/image/map/xml')
+
+    def test_map(self):
+        self._test_page('/map', 'map_canvas')
+
+    def test_fusion_latest(self):
+        self._test_page('/fusion/latest', 'slideshow')
